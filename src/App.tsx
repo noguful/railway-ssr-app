e153624b43d7e5cdfae9f9b4c7dbeb535c129ca7
noguful <noguchi.shin1@gmail.com>
2024-01-15 22:18:39 +0900

    コンテンツを反映

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import useSWR from 'swr';
+import { apiKey } from './const';
 import './App.css';
 
 function App() {
+  const fetcher = (url: string) => axios.get(url).then(res => res.data);
+  const { data, error, isLoading } = useSWR(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=2024-01-14`, fetcher);
+
+  if (error) return <div>failed to load</div>
+  if (isLoading) return <div>loading...</div>
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Astronomy Picture of the Day</h1>
       </header>
+      <main className="App-main">
+        <div className="App-image">
+          <img src={data.url} alt="" width="400" />
+        </div>
+        <section className="App-body">
+          <h2 className="App-title">{data.title}</h2>
+          <p className="App-date">{data.date}</p>
+          <p className="App-explanation">{data.explanation}</p>
+          {data.copyright && <p className="App-copyright">Image Credit & Copyright: {data.copyright}</p>}
+        </section>
+      </main>
     </div>
   );
 }
