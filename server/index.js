1d2b2e529ba4775f15e994d2d7c7964a2270f608
noguful <noguchi.shin1@gmail.com>
2024-01-22 17:41:00 +0900

    Reactをコメントアウト

--- server/index.js
+++ server/index.js
@@ -1,24 +1,29 @@
 import express from 'express';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import App from '../src/App.tsx';
+
+// import React from 'react';
+// import { renderToString } from 'react-dom/server';
+// import App from '../src/App.tsx';
 
 const app = express()
 const port = 9000
 
 app.get('/', (req, res) => {
-  const renderedHtml = renderToString(<App />);
-  res.send(`
-    <!DOCTYPE html>
-    <html lang="ja">
-    <head>
-      <title>SSRをしよう</title>
-    </head>
-    <body>
-      <div id="root">${renderedHtml}</div>
-    </body>
-    </html>
-  `);
+
+  res.send('Hello World!')
+
+  // const renderedHtml = renderToString(<App />);
+  // res.send(`
+  //   <!DOCTYPE html>
+  //   <html lang="ja">
+  //   <head>
+  //     <title>SSRをしよう</title>
+  //   </head>
+  //   <body>
+  //     <div id="root">${renderedHtml}</div>
+  //   </body>
+  //   </html>
+  // `);
+
 })
 
 app.listen(port, () => {
